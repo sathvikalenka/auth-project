7194c42bbdee6fd81fc960e80ff8457b2d5a7013
r2schools <sathvikalenka@gmail.com>
2026-05-18 10:15:55 +0000

    Initial commit - auth project

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Dashboard() {
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await axios.get("http://localhost:7000/dashboard", {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      });
+
+      alert(res.data.message);
+
+    } catch {
+      alert("Unauthorized");
+      navigate("/login");
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <div className="container">
+      <h2>Dashboard</h2>
+
+      <button onClick={getData}>Get Data</button>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
---
+++ frontend/src/Laxmi.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Project from "./Project.jsx"
+function Laxmi(){
+  return(
+    <Project />
+  )
+}
+export default Laxmi;
